b8136b96ec35cd3958c0de38e0e5d5c5b0753073
saikiran-13 <saikiran@simformsolutions.com>
2023-04-07 11:47:38 +0530

    Updated contract successfully

--- Contracts/vesting.sol
+++ Contracts/vesting.sol
@@ -3,18 +3,14 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract vesting{
 
     IERC20 public token;
-    uint public tokensLeft;
-    using safeMath for uint256;
-
 
     struct beneficiarydata{
         address addressOfToken;
-        address beneficiary;
         uint noOfTokens;
         uint cliff;
         uint startTime;
@@ -23,19 +19,16 @@ contract vesting{
         bool locked;
     }
 
-    mapping (address => beneficiarydata[]) public beneficiaryDetails; 
-    mapping (address => mapping(uint => uint)) public releasedTokens;
+    mapping (address => beneficiarydata[]) public beneficiaryDetails;
+    mapping (address => mapping(uint => uint)) public releasedTokens;//keeps track of amount of tokens by beneficiary at particular period
     mapping (address => mapping(address => bool)) public whitelist;
 
-    event tokensLocked(address Tokenaddrress,uint tokens,uint duration);
-    event tokensWithdrawn(address beneficiary,uint tokens);
-    event whitelisted(address);
-    event tokensUnLocked(uint,uint,uint,uint,uint);
+    event tokensLocked(address beneficiary,address Tokenaddress,uint tokens);
+    event tokensWithdrawn(address beneficiary,address Tokenaddress,uint tokens);
 
 
     function whitelistTokens(address _tokenaddress) external {
         whitelist[msg.sender][_tokenaddress] = true;
-        emit whitelisted(_tokenaddress);
 
     }
 
@@ -44,13 +37,12 @@ contract vesting{
     }
 
     function lockTokens(address _tokenaddress,uint _noOfTokens,uint _cliff,uint _duration,uint _sliceperiod) external{
-        
-        require(whitelist[msg.sender][_tokenaddress],"Token is not allowed");
+        require(whitelist[msg.sender][_tokenaddress],"Token not allowed");
         require(_noOfTokens>0);
         token = IERC20(_tokenaddress);
+
         beneficiarydata memory person = beneficiarydata({
             addressOfToken:_tokenaddress,
-            beneficiary:msg.sender,
             noOfTokens:_noOfTokens,
             cliff:_cliff,
             startTime: block.timestamp + _cliff,
@@ -61,39 +53,34 @@ contract vesting{
 
         beneficiaryDetails[msg.sender].push(person);
         token.transferFrom(msg.sender,address(this),_noOfTokens);
-        emit tokensLocked(_tokenaddress,_noOfTokens,beneficiaryDetails[msg.sender][0].duration);
+        emit tokensLocked(msg.sender,_tokenaddress,_noOfTokens);
     }
    
     function withdrawTokens(uint8 index) external {
-        // require(block.timestamp>beneficiaryDetails[msg.sender].startTime);
-        // require(releasedTokens[msg.sender]<beneficiaryDetails[msg.sender].noOfTokens,"Tokens already withdrawn");
-        // tokensLeft = unlockTokens(index);
-        //require(tokensLeft!=0);
+        require(block.timestamp>beneficiaryDetails[msg.sender][index].startTime,"No tokens unlocked");
+        require(releasedTokens[msg.sender][index]<beneficiaryDetails[msg.sender][index].noOfTokens,"Tokens already withdrawn");
         token = IERC20(beneficiaryDetails[msg.sender][index].addressOfToken);
-        tokensLeft=unlockTokens(index);
+        uint tokensLeft=unlockTokens(index);
+        require(tokensLeft!=0);
         token.transfer(msg.sender,tokensLeft);
         releasedTokens[msg.sender][index]+=tokensLeft;
-        //address Tokenaddrress = beneficiaryDetails[msg.sender][index].addressOfToken;
-        //emit tokensWithdrawn(msg.sender,releasedTokens[msg.sender][index]);
+        address Tokenaddress = beneficiaryDetails[msg.sender][index].addressOfToken;
+        emit tokensWithdrawn(msg.sender,Tokenaddress,releasedTokens[msg.sender][index]);
     }
     
-    function unlockTokens(uint8 ind) internal returns(uint) {
+    function unlockTokens(uint8 ind) internal view returns(uint) {
 
         uint totalNoOfPeriods = beneficiaryDetails[msg.sender][ind].duration/beneficiaryDetails[msg.sender][ind].slicePeriod;
-        uint tokensPerPeriod = beneficiaryDetails[msg.sender][ind].noOfTokens/totalNoOfPeriods;
-        require(block.timestamp>beneficiaryDetails[msg.sender][ind].startTime,"No tokens unlocked");
+        uint tokensPerPeriod = (beneficiaryDetails[msg.sender][ind].noOfTokens)/(totalNoOfPeriods);
         uint timePeriodSinceStart = block.timestamp - beneficiaryDetails[msg.sender][ind].startTime;
-        uint noOfPeriodsTillNow = timePeriodSinceStart/beneficiaryDetails[msg.sender][ind].slicePeriod;
-        uint noOfTokensTillNow = noOfPeriodsTillNow * tokensPerPeriod - releasedTokens[msg.sender][ind];
-        emit tokensUnLocked(totalNoOfPeriods,tokensPerPeriod,timePeriodSinceStart,noOfPeriodsTillNow,noOfTokensTillNow);
-       
+        uint noOfPeriodsTillNow = timePeriodSinceStart/(beneficiaryDetails[msg.sender][ind].slicePeriod);
+        uint noOfTokensTillNow = (noOfPeriodsTillNow * tokensPerPeriod) - releasedTokens[msg.sender][ind] ;
+
         if(noOfPeriodsTillNow >= totalNoOfPeriods){ //Exceeded the duration
-            return tokensPerPeriod*totalNoOfPeriods - releasedTokens[msg.sender][ind];
+            return (beneficiaryDetails[msg.sender][ind].noOfTokens) - releasedTokens[msg.sender][ind];
         }
 
-        return noOfTokensTillNow;
+        return noOfTokensTillNow ;
     }
 
-
- }//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-//
+ }
